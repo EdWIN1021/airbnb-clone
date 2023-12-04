fc3136d9274173a2ee97f6a9995ed78b44adabca
Edwin <>
2023-12-04 11:31:20 -0800

    feat: update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Property from "@/components/Property";
-import { getListings } from "./utils/server";
+import { getListings } from "@/utils/server";
 import { IListing } from "@/models/Listing";
 
 export default async function Home() {
@@ -7,7 +7,7 @@ export default async function Home() {
 
   return (
     <main>
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-6 mx-20">
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-6 px-20">
         {listings.map((listing: IListing) => (
           <Property key={listing._id} listing={listing} />
         ))}
