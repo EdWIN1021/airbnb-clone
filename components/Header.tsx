d08a25c77fb7c8eb62f27399acd691430b0552b5
Edwin <>
2023-12-04 18:55:40 -0800

    feat: responsive header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="">
       <header className="flex justify-between h-20 items-center border-b border-light-gray mx-20">
-        <div className="cursor-pointer flex-1">
+        <div className="cursor-pointer flex-1 hidden xl:block">
           <Image
             src="/logo.svg"
             style={{ width: "auto", height: "auto" }}
@@ -17,7 +17,16 @@ const Header = () => {
             priority
           />
         </div>
-
+        <div className="cursor-pointer xl:hidden min-w-[32px] min-h-[32px] mr-2">
+          <Image
+            src="/logo-sm.png"
+            style={{ width: "auto", height: "auto" }}
+            width={32}
+            height={32}
+            alt="logo"
+            priority
+          />
+        </div>
         <Search />
         <Profile />
       </header>
