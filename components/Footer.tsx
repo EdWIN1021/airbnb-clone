9371b955a110c62335dfbe3fec33d6c991ee9efe
Edwin <>
2023-12-11 20:54:24 -0800

    feat: update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -106,10 +106,11 @@ const tabs = [
 
 const Footer = () => {
   return (
-    <div className="bg-medium-gray flex justify-center mt-[150px]">
+    <div className="bg-medium-gray mt-[150px]">
       <div className="pt-12 px-20">
         <h3 className="text-[22px] mb-2">Inspiration for future getaways</h3>
-        <div className="flex gap-4 border-b border-[#DDDDDD]">
+
+        <div className="flex gap-4 border-b border-[#DDDDDD] overflow-x-auto">
           {tabs.map((tab) => (
             <span key={tab.title} className="text-dark-gray text-[14px] py-2">
               {tab.title}
@@ -117,8 +118,8 @@ const Footer = () => {
           ))}
         </div>
 
-        <div className="pt-8 pb-12">
-          <ul className="grid  gap-x-2 gap-y-6 grid-cols-[repeat(6,minmax(0,250px))]">
+        <div className="pt-8 pb-12 ">
+          <ul className="grid  gap-x-2 gap-y-6 grid-cols-[repeat(2,minmax(0,_1fr))] lg:grid-cols-[repeat(3,minmax(0,_1fr))] xl:grid-cols-[repeat(6,minmax(0,250px))] ">
             {tabs[0].cities?.map((city, index) => (
               <li className="flex flex-col text-[14px]" key={index}>
                 <span className="font-semibold">{city.name}</span>
@@ -128,7 +129,7 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="grid grid-cols-3 py-12 border-t border-[#DDDDDD] text-[14px]">
+        <div className="grid grid-cols-1 lg:grid-cols-3 py-12 border-t border-[#DDDDDD] text-[14px]">
           <div>
             <span className="font-semibold mb-3">Support</span>
             <ul className="flex flex-col gap-3">
